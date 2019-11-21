4f021753fb2d3388a7373295340fa102d7e4920c
andrerondi <andre_castro_rondi@hotmail.com>
2019-11-21 14:26:00 -0400

    Vinculando diversos arquivos PHP

--- script.php
+++ script.php
@@ -1,6 +1,11 @@
 <?php
-
-session_start();
+include "servicos/servicoMensagemSessao.php";
+include 'servicos/servicoValidacao.php';
+include "servicos/servicosCategoriaCompetidor.php";
 
 $nome = $_POST['nome'];
 $idade = $_POST['idade'];
+
+defineCategoriaCompetidor($nome, $idade);
+
+header(string 'location: index.php');
